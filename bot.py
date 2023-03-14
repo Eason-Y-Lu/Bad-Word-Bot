6273fe25281c8cd4291d7f94d3d1726146bf56eb
Eason Lu <github@easonlu.ml>
2023-03-14 13:05:17 -0700

    Update bot.py
    Signed-off-by: Eason Lu <github@easonlu.ml>

--- bot.py
+++ bot.py
@@ -18,26 +18,34 @@ async def on_message(message):
   if message.author.bot or isinstance(message.channel, discord.DMChannel):
     return
 
-  profanity_words = ['badword1', 'badword2', 'badword3']
+  with open('profanity.txt', 'r') as file:
+    profanity_words = [word.strip() for word in file]
 
   for word in profanity_words:
     # Use regular expressions to match the profanity word regardless of its position in the message
     regex_pattern = r"\b\w*" + re.escape(word) + r"\w*\b"
     regex_match = re.search(regex_pattern, message.content, re.IGNORECASE)
-    
+
     if regex_match:
       filtered_word = word[0] + '*' * (len(word) - 2) + word[-1]
-      filtered_message = re.sub(regex_pattern, filtered_word, message.content, flags=re.IGNORECASE)
+      filtered_message = re.sub(regex_pattern,
+                                filtered_word,
+                                message.content,
+                                flags=re.IGNORECASE)
       filtered_message = filtered_message.replace(f"<@{message.author.id}>",
                                                   '{user said}')
       await message.delete()
       await message.channel.send(f"{message.author}: {filtered_message}")
       break
 
+  # Process commands after checking for profanity words
+  await bot.process_commands(message)
+
 
 @bot.command()
 async def online(ctx):
-  await ctx.send(f"The bot is currently {str(bot.latency*1000)}ms latency and online.")
+  await ctx.send(
+    f"The bot is currently {str(bot.latency*1000)}ms latency and online.")
 
 
 bot.run(my_secret)
