96a42f6373d8e36dc7cc34557fd17cf5ac27dfcc
Eason Lu <github@easonlu.ml>
2023-03-14 10:33:38 -0700

    Update bot.py
    Signed-off-by: Eason Lu <github@easonlu.ml>

--- bot.py
+++ bot.py
@@ -1,44 +1,48 @@
+import os
 import discord
 import re
-import os
-
-from discord import Intents
 from discord.ext import commands
 
-intents = discord.Intents.all()
-client = discord.Client(command_prefix="!", intents=intents)
-
-
-client = commands.Bot(command_prefix='~', intents=intents)
 my_secret = os.environ['DISCORD_BOT_TOKEN']
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="~", intents=intents)
 
 
-@client.event
+@bot.event
 async def on_ready():
-    print('Logged in as {0.user}'.format(client))
+  print('Logged in as {0.user}'.format(bot))
 
 
-@client.event
-async def on_message(message):
-    if message.author.bot or isinstance(message.channel, discord.DMChannel):
-        return
-
-    profanity_words = ['badword1', 'badword2', 'badword3']
-
-    pattern = re.compile(r'\b(?:%s)\b' % '|'.join(profanity_words), re.IGNORECASE)
-    if pattern.search(message.content):
-        filtered_message = pattern.sub(lambda match: match.group(1) + '*' * (len(match.group(0)) - 2) + match.group(-1), message.content)
-        filtered_message = filtered_message.replace(f"<@{message.author.id}>", '{user said}')
-        await message.delete()
-        await message.channel.send(f"{message.author}: {filtered_message}")
-
-        dm_channel = await message.author.create_dm()
-        await dm_channel.send(f"Hi {message.author.name}, please refrain from using profanity in this server. Thank you!")
-
-
-@client.command()
+@bot.command()
 async def online(ctx):
-    await ctx.send(f"The bot is online and ready to filter profanity!")
+  await ctx.send(f"The bot is online and ready to filter profanity!")
 
 
-client.run(my_secret)
+@bot.event
+async def on_message(message):
+  if message.author.bot or isinstance(message.channel, discord.DMChannel):
+    return
+
+  profanity_words = ['badword1', 'badword2', 'badword3']
+
+  for word in profanity_words:
+    if word in message.content.lower():
+      filtered_word = word[0] + '*' * (len(word) - 2) + word[-1]
+      filtered_message = re.sub(r'\b{}\b'.format(word),
+                                filtered_word,
+                                message.content,
+                                flags=re.IGNORECASE)
+
+      filtered_message = filtered_message.replace(f"<@{message.author.id}>",
+                                                  '{user said}')
+      await message.delete()
+      await message.channel.send(f"{message.author}: {filtered_message}")
+
+      dm_channel = await message.author.create_dm()
+      await dm_channel.send(
+        f"Hi {message.author.name}, please refrain from using profanity in this server. Thank you!"
+      )
+      break
+
+
+bot.run(my_secret)
