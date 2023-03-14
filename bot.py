0b27ed80cbf4e73ab714f5b0ce328ff561e5cbe1
Eason Lu <github@easonlu.ml>
2023-03-14 10:52:51 -0700

    Update bot.py
    Signed-off-by: Eason Lu <github@easonlu.ml>

--- bot.py
+++ bot.py
@@ -24,23 +24,20 @@ async def on_message(message):
     # Use regular expressions to match the profanity word regardless of its position in the message
     regex_pattern = r"\b\w*" + re.escape(word) + r"\w*\b"
     regex_match = re.search(regex_pattern, message.content, re.IGNORECASE)
-
+    
     if regex_match:
       filtered_word = word[0] + '*' * (len(word) - 2) + word[-1]
-      filtered_message = re.sub(regex_pattern,
-                                filtered_word,
-                                message.content,
-                                flags=re.IGNORECASE)
+      filtered_message = re.sub(regex_pattern, filtered_word, message.content, flags=re.IGNORECASE)
       filtered_message = filtered_message.replace(f"<@{message.author.id}>",
                                                   '{user said}')
       await message.delete()
       await message.channel.send(f"{message.author}: {filtered_message}")
-
-      dm_channel = await message.author.create_dm()
-      await dm_channel.send(
-        f"Hi {message.author.name}, please refrain from using profanity in this server. Thank you!"
-      )
       break
+
+
+@bot.command()
+async def online(ctx):
+  await ctx.send(f"The bot is currently {str(bot.latency*1000)}ms latency and online.")
 
 
 bot.run(my_secret)
