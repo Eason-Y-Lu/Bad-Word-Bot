aff103d2a01e6a6b55e65ea74acd2531392abac9
Eason Lu <github@easonlu.ml>
2023-03-14 09:29:15 -0700

    Update bot.py
    Signed-off-by: Eason Lu <github@easonlu.ml>

--- bot.py
+++ bot.py
@@ -3,11 +3,12 @@ import re
 import os
 
 from discord import Intents
+from discord.ext import commands
 
 intents = Intents.default()
 intents.members = True
 
-client = discord.Client(intents=intents)
+client = commands.Bot(command_prefix='~', intents=intents)
 my_secret = os.environ['DISCORD_BOT_TOKEN']
 
 
@@ -30,9 +31,13 @@ async def on_message(message):
         await message.delete()
         await message.channel.send(f"{message.author}: {filtered_message}")
 
-        # Send a direct message to the user who sent the profanity
         dm_channel = await message.author.create_dm()
         await dm_channel.send(f"Hi {message.author.name}, please refrain from using profanity in this server. Thank you!")
-        
+
+
+@client.command()
+async def online(ctx):
+    await ctx.send(f"The bot is online and ready to filter profanity!")
+
 
 client.run(my_secret)
