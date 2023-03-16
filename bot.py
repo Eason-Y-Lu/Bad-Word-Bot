28c594a49c2f1bd457e93756d465e77789c911d2
Eason Lu <github@easonlu.ml>
2023-03-16 10:49:13 -0700

    Update bot.py (#4)
    Signed-off-by: Eason Lu <github@easonlu.ml>

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 import discord
 import re
+import wikipedia  # Added this line to import wikipedia
 from discord.ext import commands
 
 intents = discord.Intents.all()
@@ -8,7 +9,7 @@ bot = commands.Bot(command_prefix="~", intents=intents)
 
 @bot.command()
 async def ping(ctx):
-    await ctx.send(f"Pong! The bot is currently {str(bot.latency*1000)}ms latency and online.")
+    await ctx.send(f"The bot is currently {str(bot.latency*1000)}ms latency and online.")
 
 
 @bot.event
@@ -43,6 +44,17 @@ async def on_message(message):
 
     # Process commands after checking for profanity words
     await bot.process_commands(message)
+
+
+@bot.command()
+async def wiki(ctx, *, query):
+    try:
+        summary = wikipedia.summary(query, sentences=3)
+        await ctx.send(summary)
+    except wikipedia.exceptions.DisambiguationError as e:
+        await ctx.send(f"Multiple results were found. Please be more specific.")
+    except wikipedia.exceptions.PageError as e:
+        await ctx.send(f"No results were found for \"{query}\".")
 
 
 bot.run('#redacted')
