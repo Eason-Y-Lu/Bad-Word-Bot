162621b48b905a3561fe672b960c19173a7c295a
Eason Lu <github@easonlu.ml>
2023-03-16 09:53:25 -0700

    Update bot.py
    Signed-off-by: Eason Lu <github@easonlu.ml>

--- bot.py
+++ bot.py
@@ -1,28 +1,7 @@
 import os
 import discord
 import re
-import itertools
-from discord.ext import commands, tasks
-
-from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-
-@app.route('/')
-def main():
-  return "Your Bot Is Ready"
-
-
-def run():
-  app.run(host="0.0.0.0", port=8000)
-
-
-def keep_alive():
-  server = Thread(target=run)
-  server.start()
-
+from discord.ext import commands
 
 my_secret = os.environ['DISCORD_BOT_TOKEN']
 intents = discord.Intents.all()
@@ -31,51 +10,41 @@ bot = commands.Bot(command_prefix="~", intents=intents)
 
 @bot.command()
 async def online(ctx):
-  await ctx.send(
-    f"The bot is currently {str(bot.latency*1000)}ms latency and online.")
+    await ctx.send(f"The bot is currently {str(bot.latency*1000)}ms latency and online.")
 
 
 @bot.event
 async def on_ready():
-  print('Logged in as {0.user}'.format(bot))
-  change_status.start()
+    print('Logged in as {0.user}'.format(bot))
 
 
 @bot.event
 async def on_message(message):
-  if message.author.bot or isinstance(message.channel, discord.DMChannel):
-    return
+    if message.author.bot or isinstance(message.channel, discord.DMChannel):
+        return
 
-  with open('profanity.txt', 'r') as file:
-    profanity_words = [word.strip() for word in file]
+    with open('profanity.txt', 'r') as file:
+        profanity_words = [word.strip() for word in file]
 
-  for word in profanity_words:
-    # Use regular expressions to match the profanity word regardless of its position in the message
-    regex_pattern = r"\b\w*" + re.escape(word) + r"\w*\b"
-    regex_match = re.search(regex_pattern, message.content, re.IGNORECASE)
+    for word in profanity_words:
+        # Use regular expressions to match the profanity word regardless of its position in the message
+        regex_pattern = r"\b\w*" + re.escape(word) + r"\w*\b"
+        regex_match = re.search(regex_pattern, message.content, re.IGNORECASE)
 
-    if regex_match:
-      filtered_word = word[0] + '*' * (len(word) - 2) + word[-1]
-      filtered_message = re.sub(regex_pattern,
-                                filtered_word,
-                                message.content,
-                                flags=re.IGNORECASE)
-      filtered_message = filtered_message.replace(f"<@{message.author.id}>",
-                                                  '{user said}')
-      await message.delete()
-      await message.channel.send(f"{message.author}: {filtered_message}")
-      break
+        if regex_match:
+            filtered_word = word[0] + '*' * (len(word) - 2) + word[-1]
+            filtered_message = re.sub(regex_pattern,
+                                      filtered_word,
+                                      message.content,
+                                      flags=re.IGNORECASE)
+            filtered_message = filtered_message.replace(f"<@{message.author.id}>",
+                                                        '{user said}')
+            await message.delete()
+            await message.channel.send(f"{message.author}: {filtered_message}")
+            break
 
-  # Process commands after checking for profanity words
-  await bot.process_commands(message)
-
-
-status = itertools.cycle(['with Python', 'JetHub'])
-
-
-@tasks.loop(seconds=10)
-async def change_status():
-  await bot.change_presence(activity=discord.Game(next(status)))
+    # Process commands after checking for profanity words
+    await bot.process_commands(message)
 
 
 bot.run(my_secret)
