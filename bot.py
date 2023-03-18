5e5e0498466c5d86c1ba5b87dbd274be6aeaf847
Eason Lu <github@easonlu.ml>
2023-03-18 00:13:28 -0700

    Update bot.py
    Signed-off-by: Eason Lu <github@easonlu.ml>

--- bot.py
+++ bot.py
@@ -17,17 +17,18 @@ async def ping(ctx):
 
 @bot.command()
 async def profanity(ctx):
-    with open('profanity.txt', 'r') as file:
+    guild_id = ctx.guild.id
+    with open(f'profanity_{guild_id}.txt', 'r') as file:
         profanity_words = [word.strip() for word in file]
     if profanity_words:
         if sum(len(word) for word in profanity_words) <= 1900:
             await ctx.send("Current profanity list:\n" + "\n".join(profanity_words))
         else:
-            with open('profanity_list.txt', 'w') as f:
+            with open(f'profanity_list_{guild_id}.txt', 'w') as f:
                 f.write("\n".join(profanity_words))
-            with open('profanity_list.txt', 'rb') as f:
+            with open(f'profanity_list_{guild_id}.txt', 'rb') as f:
                 await ctx.send("Current profanity list is too long to display. Here's a text file with the full list:",
-                               file=discord.File(f, 'profanity_list.txt'))
+                               file=discord.File(f, f'profanity_list_{guild_id}.txt'))
     else:
         await ctx.send("The profanity list is currently empty.")
 
@@ -35,7 +36,8 @@ async def profanity(ctx):
 @bot.command()
 @commands.has_role('Office of Strategic Services')
 async def add(ctx, word):
-    with open('profanity.txt', 'a') as file:
+    guild_id = ctx.guild.id
+    with open(f'profanity_{guild_id}.txt', 'a') as file:
         file.write(word + "\n")
     await ctx.send(f"Added \"{word}\" to the profanity list.")
 
@@ -43,15 +45,17 @@ async def add(ctx, word):
 @bot.command()
 @commands.has_role('Office of Strategic Services')
 async def remove(ctx, word):
-    with open('profanity.txt', 'r') as file:
+    guild_id = ctx.guild.id
+    with open(f'profanity_{guild_id}.txt', 'r') as file:
         profanity_words = [w.strip() for w in file]
     if word in profanity_words:
         profanity_words.remove(word)
-        with open('profanity.txt', 'w') as file:
+        with open(f'profanity_{guild_id}.txt', 'w') as file:
             file.write("\n".join(profanity_words))
         await ctx.send(f"Removed the following word from the profanity list: \"{word}\"")
     else:
         await ctx.send(f"\"{word}\" was not found in the profanity list.")
+
 
 @bot.command()
 async def role(ctx):
@@ -59,7 +63,7 @@ async def role(ctx):
     user_roles = [role[1:] if role.startswith("@") else role for role in user_roles]
     await ctx.send(f"You have the following roles: {', '.join(user_roles)}")
 
-        
+
 @bot.event
 async def on_ready():
     print('Logged in as {0.user}'.format(bot))
@@ -76,8 +80,15 @@ async def on_message(message):
         await bot.process_commands(message)
         return
 
-    with open('profanity.txt', 'r') as file:
-        profanity_words = [word.strip() for word in file]
+    # Get the name of the profanity list file based on the server name
+    profanity_file = f"{message.guild.name}_profanity.txt"
+
+    # Check if the profanity list file exists
+    if os.path.isfile(profanity_file):
+        with open(profanity_file, 'r') as file:
+            profanity_words = [word.strip() for word in file]
+    else:
+        profanity_words = []
 
     for word in profanity_words:
         # Use regular expressions to match the profanity word regardless of its position in the message
@@ -98,5 +109,7 @@ async def on_message(message):
 
     # Process commands after checking for profanity words
     await bot.process_commands(message)
+
+
 
 bot.run('#redacted')
